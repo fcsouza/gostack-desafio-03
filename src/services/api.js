ec351ea1b796ac9db0faea3d630068bb35efae8e
fcsouza <fcsouza18@hotmail.com>
2020-04-21 23:01:58 -0300

    desafio mobile

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,14 @@
-import axios from "axios";
+import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "http://localhost:3333",
+  baseURL:'http://10.0.0.104:3333',
 });
 
 export default api;
+
+/**
+ * Android com emulador: localhost (adb reverse)
+ * Android com emulador: 10.0.2.2 (Android Studio)
+ * Android com emulador: 10.0.3.2 (Genymotion)
+ * Android físico: IP da máquina 
+ */
